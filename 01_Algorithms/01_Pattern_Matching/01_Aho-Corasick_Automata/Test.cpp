5b632faf0be42e2761e6864a6d8f5aa0f6f13654
unsik6 <tlsguswns119@naver.com>
2022-07-07 19:01:59 +0900

    df

---
+++ 01_Algorithms/01_Pattern_Matching/01_Aho-Corasick_Automata/Test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <queue>
+#include <string>
+#include "AC_Automata.h"
+
+using namespace std;
+
+class BIRD_2D
+{
+	int m_txtSize = 0; int m_patSize = 0;
+	string* m_txt = nullptr;
+	string* m_patSize;
+
+	AC_Automata m_ac;
+	int* m_kmp_DP_col = nullptr;
+	
+	BIRD_2D(string* _pattern, int _tSize)
+	{
+	}
+
+
+};
+
+
+int main()
+{
+	
+
+	return 0;
+}
