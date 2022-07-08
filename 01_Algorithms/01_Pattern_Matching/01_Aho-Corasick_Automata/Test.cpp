86c6ad6db2f1631c969bb67a30e9b3a4bf45e215
unsik6 <80208196+unsik6@users.noreply.github.com>
2022-07-08 16:38:38 +0900

    Update Test.cpp

--- 01_Algorithms/01_Pattern_Matching/01_Aho-Corasick_Automata/Test.cpp
+++ 01_Algorithms/01_Pattern_Matching/01_Aho-Corasick_Automata/Test.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
-#include <vector>
-#include <list>
-#include <queue>
-#include <string>
 #include "AC_Automata.h"
 
 using namespace std;
 
-class BIRD_2D
-{
-	int m_txtSize = 0; int m_patSize = 0;
-	string* m_txt = nullptr;
-	string* m_patSize;
 
-	AC_Automata m_ac;
-	int* m_kmp_DP_col = nullptr;
-	
-	BIRD_2D(string* _pattern, int _tSize)
-	{
-	}
-
-
-};
 
 
 int main()
 {
-	
+	vector<string> patterns;
+	string curPat;
+	cin >> curPat;
+	while (curPat != "Q")
+	{
+		patterns.push_back(curPat);
+		cin >> curPat;
+	}
+
+	AC_Automata ac(&patterns);
+	string text;
+	cin >> text;
+
+	ac.printForCheck();
+	cout << endl;
+	ac.patternMatching(text);
+
 
 	return 0;
 }
